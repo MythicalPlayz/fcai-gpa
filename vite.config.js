eef9669f6a11a0536e489601e669a4761ede7a45
MythicalPlayz <eyaddodo05@gmail.com>
2025-07-25 00:52:24 +0300

    final init

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/fcai-gpa/',
   plugins: [
     react(),
     tailwindcss(),
@@ -11,18 +12,18 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
-        name: 'My PWA App',
-        short_name: 'PWA App',
-        description: 'My awesome single-page PWA app!',
-        theme_color: '#ffffff',
+        name: 'FCAI GPA+',
+        short_name: 'FCAI GPA+',
+        description: 'GPA Calculator for FCAI Students',
+        theme_color: '#2b7fff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'pwa-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'pwa-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
